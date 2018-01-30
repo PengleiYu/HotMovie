c6e1e80b0be61abe7ea5fd80bcb9f4225574283b
penglei <yupengniaosanshi@gmail.com>
2018-01-30 17:13:29 +0800

    save

---
+++ app/src/main/java/com/example/penglei/hotmovie/utilities/JsonUtil.java
@@ -0,0 +1,13 @@
+package com.example.penglei.hotmovie.utilities;
+
+import android.content.Context;
+
+/**
+ * Created by penglei on 18-1-30.
+ */
+
+public class JsonUtil {
+    public static String[] getSimpleMovieStringsFromJson(Context context,String hotMovieJsonStr){
+        return null;
+    }
+}
