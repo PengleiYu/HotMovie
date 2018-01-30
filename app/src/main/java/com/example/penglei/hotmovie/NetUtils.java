e559b9d707c5ee7ac95b017617587a1b93da2314
penglei <yupengniaosanshi@gmail.com>
2018-01-30 12:04:38 +0800

    添加网络工具类

---
+++ app/src/main/java/com/example/penglei/hotmovie/NetUtils.java
@@ -0,0 +1,48 @@
+package com.example.penglei.hotmovie;
+
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by penglei on 18-1-30.
+ */
+
+public class NetUtils {
+    private static final String API_KEY = "02dfef023bf0bd258e9fc6f06b0a8e5d";
+    private static final String STATIC_MOVIE_URL = "http://api.themoviedb.org/3/movie";
+    private static final String POPULAR_URL = STATIC_MOVIE_URL + "/popular";
+    private static final String PARAM_API_KEY = "api_key";
+
+    @Nullable
+    public static URL buildUrl() {
+        Uri uri = Uri.parse(POPULAR_URL).buildUpon()
+                .appendQueryParameter(PARAM_API_KEY, API_KEY)
+                .build();
+        try {
+            return new URL(uri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static String getResponseFromUrl(URL url) throws IOException {
+        HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+        try {
+            InputStream inputStream=connection.getInputStream();
+            Scanner scanner=new Scanner(inputStream);
+            scanner.useDelimiter("\\A");
+            if (scanner.hasNext()){
+                  return scanner.next();
+            }else return null;
+        }finally {
+            connection.disconnect();
+        }
+    }
+}
