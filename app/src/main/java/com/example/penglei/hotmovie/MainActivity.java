04a88a67ef31397942f0eaf7ff30bf648eb3a55b
penglei <yupengniaosanshi@gmail.com>
2018-01-30 16:57:38 +0800

    请求成功

--- app/src/main/java/com/example/penglei/hotmovie/MainActivity.java
+++ app/src/main/java/com/example/penglei/hotmovie/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.penglei.hotmovie;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.text_main);
+        new MovieTask().execute();
+    }
+
+    private class MovieTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            URL url = NetUtils.buildUrl();
+            try {
+                return NetUtils.getResponseFromUrl(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            mTextView.setText(s);
+        }
     }
 }
