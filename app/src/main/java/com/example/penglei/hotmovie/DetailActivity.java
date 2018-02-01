e511a8262c00fc15e8c1d79e95a961777cacca07
penglei <yupengniaosanshi@gmail.com>
2018-02-02 00:17:45 +0800

    详情页添加分享按钮

--- app/src/main/java/com/example/penglei/hotmovie/DetailActivity.java
+++ app/src/main/java/com/example/penglei/hotmovie/DetailActivity.java
@@ -1,8 +1,12 @@
 package com.example.penglei.hotmovie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 /**
@@ -10,13 +14,34 @@ import android.widget.TextView;
  */
 
 public class DetailActivity extends AppCompatActivity {
+    private static final String MOVIE_SHARE_HASHTAG = "#HotMovie";
+    private String mMovie;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         TextView textView = findViewById(R.id.tv_display_movie);
         if (getIntent().hasExtra(MainActivity.EXTRA_MOVIE)) {
-            textView.setText(getIntent().getStringExtra(MainActivity.EXTRA_MOVIE));
+            mMovie = getIntent().getStringExtra(MainActivity.EXTRA_MOVIE);
+            textView.setText(mMovie);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem item = menu.findItem(R.id.action_share);
+        /* 第一次发现还有这操作 */
+        item.setIntent(getSharedIntent());
+        return true;
+    }
+
+    private Intent getSharedIntent() {
+        /* 留意用法 */
+        return ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mMovie + MOVIE_SHARE_HASHTAG)
+                .getIntent();
     }
 }
