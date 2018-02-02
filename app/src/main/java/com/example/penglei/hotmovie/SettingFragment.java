2f53575ed3cb9cb60c17c2d17ae362f17831307f
penglei <yupengniaosanshi@gmail.com>
2018-02-02 17:01:29 +0800

    完成设置页，模仿天气app

---
+++ app/src/main/java/com/example/penglei/hotmovie/SettingFragment.java
@@ -0,0 +1,70 @@
+package com.example.penglei.hotmovie;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
+
+/**
+ * Created by yupenglei on 18/2/2.
+ */
+
+public class SettingFragment extends PreferenceFragmentCompat
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.pref_general);
+        PreferenceScreen screen = getPreferenceScreen();
+        SharedPreferences sharedPreferences = screen.getSharedPreferences();
+
+        /* 遍历每一个偏好条目，查找在分享偏好中存储的值，并设置到偏好中 */
+        for (int i = 0; i < screen.getPreferenceCount(); i++) {
+            Preference preference = screen.getPreference(i);
+            if (preference instanceof CheckBoxPreference) continue;
+            String value = sharedPreferences.getString(preference.getKey(), "");
+            setPreferenceSummary(preference, value);
+        }
+    }
+
+    /**
+     * 当sp发生变化时，查找相应的偏好进行设置
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (preference != null && !(preference instanceof CheckBoxPreference)) {
+            setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * 为不同的{@link Preference}设置value
+     */
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            /* 找到要设置的值在值表中的位置；若不存在则不设置，即自动显示默认值 */
+            int index = listPreference.findIndexOfValue(value);
+            if (index >= 0) {
+                listPreference.setSummary(listPreference.getEntryValues()[index]);
+            }
+        } else {
+            preference.setSummary(value);
+        }
+    }
+}
